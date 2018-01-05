2576383e9d4b15f0fe8fc0edb57f033cc99526bf
Kjetil Svalestuen <kjetil@dhis2.org>
2018-01-05 09:25:41 +0100

    Fixed bug where headers were not set correctly in production

--- app/src/scheduler.js
+++ app/src/scheduler.js
@@ -11,8 +11,7 @@ getManifest('./manifest.webapp')
     .then(manifest => `${manifest.getBaseUrl()}/api`)
     .catch(() => BASE_URL)
     .then(baseUrl => {
-        const production = process.env.NODE === 'production';
-
+        const production = process.env.NODE_ENV === 'production';
         const config = {
             baseUrl,
             headers: production ? null : SYSTEM_AUTH,
--- webpack.dev.js
+++ webpack.dev.js
@@ -8,9 +8,7 @@ module.exports = merge(common, {
     },
     plugins: [
         new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('development'),
-            },
+            'process.env.NODE_ENV': JSON.stringify('development'),
         }),
     ],
     devServer: {
