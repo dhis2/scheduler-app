aed07acc222cf079c48e5444469d10829e43ec0a
ismay <ismay@ismaywolff.nl>
2019-06-21 14:02:48 +0200

    chore: start from basic create-react-app

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { CssReset } from '@dhis2/ui-core'
+import { HeaderBar } from '@dhis2/ui-widgets'
+import { DataProvider } from '@dhis2/app-runtime'
+import Routes from '../Routes';
+
+const { REACT_APP_DHIS2_BASE_URL } = process.env;
+
+const App = () => (
+  <DataProvider baseUrl={REACT_APP_DHIS2_BASE_URL} apiVersion="">
+    <CssReset />
+    <HeaderBar />
+    <Routes />
+  </DataProvider>
+);
+
+export default App;
