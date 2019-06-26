28a93afa4002028a93cda92ebcefc1c36c1ec0c2
ismay <ismay@ismaywolff.nl>
2019-06-26 15:42:33 +0200

    build: add eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,29 @@ const SEVERITY = 2
 
 module.exports = {
     root: true,
-
     parser: 'babel-eslint',
-
     env: {
         browser: true,
         node: true,
         jest: true,
     },
-
     parserOptions: {
-        // latest standard is ok, eq. to 9
         ecmaVersion: 2018,
         ecmaFeatures: {
             jsx: true,
             modules: true,
         },
     },
-
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:jsx-a11y/recommended',
+        'plugin:compat/recommended',
+        'prettier',
+        'prettier/react',
+    ],
     rules: {
         'max-params': [
             SEVERITY,
@@ -34,6 +39,11 @@ module.exports = {
                 ignoreReadBeforeAssign: false,
             },
         ],
-        'no-mixed-spaces-and-tabs': [SEVERITY],
+        'import/order': ['error', { 'newlines-between': 'never' }],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
 }
