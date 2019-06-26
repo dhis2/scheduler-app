894af1eb7986e37c64dec49a0aa278f9e545daaf
ismay <ismay@ismaywolff.nl>
2019-06-26 15:11:15 +0200

    build: add cli-style

--- src/pages/JobEdit/JobEdit.js
+++ src/pages/JobEdit/JobEdit.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from 'react'
 
-const JobEdit = () => (
-  <div>Edit job</div>
-);
+const JobEdit = () => <div>Edit job</div>
 
-export default JobEdit;
+export default JobEdit
