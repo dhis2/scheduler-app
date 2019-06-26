1ff3335cc67c17eae5623d73e0584809b344cdab
ismay <ismay@ismaywolff.nl>
2019-06-26 17:05:22 +0200

    build: add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+    plugins: ['stylelint-no-unsupported-browser-features'],
+    rules: {
+        'font-family-name-quotes': 'always-where-recommended',
+        'color-named': 'never',
+        'declaration-no-important': true,
+        'font-weight-notation': 'numeric',
+        'plugin/no-unsupported-browser-features': [
+            true,
+            {
+                severity: 'warning',
+            },
+        ],
+    },
+}
