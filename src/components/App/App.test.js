104109f9335c4ab5e176b2dbcd103ccea804f787
ismay <ismay@ismaywolff.nl>
2019-06-26 17:44:22 +0200

    test: add testing libraries

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from './App'
 
-it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
-    ReactDOM.unmountComponentAtNode(div)
+describe('<App>', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div')
+
+        ReactDOM.render(<App />, div)
+        ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('renders correctly', () => {
+        const wrapper = shallow(<App />)
+
+        expect(wrapper).toMatchSnapshot()
+    })
 })
