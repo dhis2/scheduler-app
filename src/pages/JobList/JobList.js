4f1b16f7f18bad5d29f2e9452f342743a4642868
ismay <ismay@ismaywolff.nl>
2019-07-01 15:18:56 +0200

    refactor: change wrapper to pagewrapper

--- src/pages/JobList/JobList.js
+++ src/pages/JobList/JobList.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Card } from '@dhis2/ui-core'
-import Wrapper from '../../components/Wrapper'
 import Title from '../../components/Title'
 
 const JobList = () => (
-    <Wrapper>
+    <React.Fragment>
         <Title priority={2}>Scheduled Jobs</Title>
         <Card>Some content here</Card>
-    </Wrapper>
+    </React.Fragment>
 )
 
 export default JobList
