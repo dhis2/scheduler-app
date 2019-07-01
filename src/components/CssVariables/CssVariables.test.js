2a63b87a26e8c04c2a834c937e2ebcb0ad0e8733
ismay <ismay@ismaywolff.nl>
2019-07-01 14:01:16 +0200

    test: remove unsupported tests

--- src/components/CssVariables/CssVariables.test.js
+++ src/components/CssVariables/CssVariables.test.js
@@ -1,42 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
-import { theme, colors } from '@dhis2/ui-core'
 import CssVariables from './CssVariables'
 
 describe('<CssVariables>', () => {
-    it('inserts css theme variables', () => {
-        const div = document.createElement('div')
-        const root = document.documentElement
-
-        ReactDOM.render(<CssVariables />, div)
-
-        Object.keys(theme).forEach(key => {
-            const value = getComputedStyle(root).getPropertyValue(
-                `--theme-${key}`
-            )
-            expect(value).toEqual(theme[key])
-        })
-
-        ReactDOM.unmountComponentAtNode(div)
-    })
-
-    it('inserts css color variables', () => {
-        const div = document.createElement('div')
-        const root = document.documentElement
-
-        ReactDOM.render(<CssVariables />, div)
-
-        Object.keys(colors).forEach(key => {
-            const value = getComputedStyle(root).getPropertyValue(
-                `--colors-${key}`
-            )
-            expect(value).toEqual(colors[key])
-        })
-
-        ReactDOM.unmountComponentAtNode(div)
-    })
-
     it('renders correctly', () => {
         const wrapper = shallow(<CssVariables />)
 
