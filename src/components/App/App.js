c1d229dbe60c7a29f6befccfc3408ef534527ab4
ismay <ismay@ismaywolff.nl>
2019-07-01 14:51:28 +0200

    revert: remove css-variables component for the moment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,12 @@ import { CssReset } from '@dhis2/ui-core'
 import { HeaderBar } from '@dhis2/ui-widgets'
 import { DataProvider } from '@dhis2/app-runtime'
 import Routes from '../Routes'
-import CssVariables from '../CssVariables'
 
 const { REACT_APP_DHIS2_BASE_URL } = process.env
 
 const App = () => (
     <DataProvider baseUrl={REACT_APP_DHIS2_BASE_URL} apiVersion="">
         <CssReset />
-        <CssVariables />
         <HeaderBar appName="Scheduler" />
         <Routes />
     </DataProvider>
