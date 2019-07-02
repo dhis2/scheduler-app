a0fdb5190f4270e14e78639293052702eaa78a9d
ismay <ismay@ismaywolff.nl>
2019-07-02 10:47:17 +0200

    chore: add new job link

--- src/pages/JobList/JobList.js
+++ src/pages/JobList/JobList.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Card } from '@dhis2/ui-core'
+import { Link } from 'react-router-dom'
 import { Title } from '../../components/Title'
 
 const JobList = () => (
     <React.Fragment>
         <Title priority={2}>Scheduled Jobs</Title>
-        <Card>Some content here</Card>
+        <Card>
+            <Link to="/add">New job</Link>
+        </Card>
     </React.Fragment>
 )
 
