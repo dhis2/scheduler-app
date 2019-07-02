798271e550433ac93daa982021368231c47de87c
ismay <ismay@ismaywolff.nl>
2019-07-02 17:42:09 +0200

    chore: add data to joblist

--- src/pages/JobList/JobList.js
+++ src/pages/JobList/JobList.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import { arrayOf, shape, string } from 'prop-types'
 import { Card } from '@dhis2/ui-core'
 import { Link } from 'react-router-dom'
 import { Title } from '../../components/Title'
 
-const JobList = () => (
+const JobList = ({ jobs }) => (
     <React.Fragment>
         <Title priority={2}>Scheduled Jobs</Title>
         <Card>
             <Link to="/add">New job</Link>
+            {jobs.map(({ id, displayName }) => (
+                <div key={id}>{displayName}</div>
+            ))}
         </Card>
     </React.Fragment>
 )
+
+JobList.propTypes = {
+    jobs: arrayOf(
+        shape({
+            id: string,
+            displayName: string,
+        })
+    ).isRequired,
+}
 
 export default JobList
