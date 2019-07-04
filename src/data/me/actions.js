e611f033a809dfe8c0ae54299857c834311239b5
ismay <ismay@ismaywolff.nl>
2019-07-04 10:37:24 +0200

    chore: add redux for state management

---
+++ src/data/me/actions.js
@@ -0,0 +1,42 @@
+import fetchy from '../../services/fetchy'
+import * as rootSelectors from '../../rootSelectors'
+import * as types from './actionTypes'
+import * as selectors from './reducer'
+
+const endpoint = `${process.env.REACT_APP_DHIS2_BASE_URL}/api/me`
+
+export const fetchMeSuccess = payload => ({
+    type: types.FETCH_ME_SUCCESS,
+    meta: {
+        receivedAt: Date.now(),
+    },
+    payload,
+})
+
+export const fetchMeFail = error => ({
+    type: types.FETCH_ME_FAIL,
+    meta: {
+        receivedAt: Date.now(),
+    },
+    error,
+})
+
+export const fetchMe = () => dispatch => {
+    dispatch({ type: types.FETCH_ME })
+
+    return fetchy(endpoint, { credentials: 'include' })
+        .then(response => response.json())
+        .then(data => dispatch(fetchMeSuccess(data)))
+        .catch(error => dispatch(fetchMeFail(error)))
+}
+
+export const fetchMeIfNeeded = () => (dispatch, getState) => {
+    const state = rootSelectors.getMe(getState())
+    const shouldFetch = selectors.getShouldFetch(state)
+
+    if (shouldFetch) {
+        return dispatch(fetchMe())
+    }
+
+    return Promise.resolve()
+}
