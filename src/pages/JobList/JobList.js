08e82a9a646069d3fa89f1f064b6eb0233de8d0d
ismay <ismay@ismaywolff.nl>
2019-07-08 12:13:13 +0200

    fix: fix prop types

--- src/pages/JobList/JobList.js
+++ src/pages/JobList/JobList.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { object, arrayOf, string } from 'prop-types'
 import { Card } from '@dhis2/ui-core'
 import { Link } from 'react-router-dom'
 import { Title } from '../../components/Title'
 import { Info } from '../../components/Icons'
+import JobListItem from './JobListItem'
 
-const JobList = () => (
+const JobList = ({ jobIds, jobEntities }) => (
     <React.Fragment>
         <Title priority={2}>Scheduled Jobs</Title>
         <Info />
@@ -21,10 +23,19 @@ const JobList = () => (
                         <th>On/off</th>
                     </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody>
+                    {jobIds.map(id => (
+                        <JobListItem key={id} job={jobEntities[id]} />
+                    ))}
+                </tbody>
             </table>
         </Card>
     </React.Fragment>
 )
+
+JobList.propTypes = {
+    jobIds: arrayOf(string).isRequired,
+    jobEntities: object.isRequired,
+}
 
 export default JobList
