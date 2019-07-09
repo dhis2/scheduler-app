fc51cdce498a06bae869f507fcc6696aba1081aa
ismay <ismay@ismaywolff.nl>
2019-07-09 12:11:42 +0200

    test: update tests

--- src/data/jobs/actions.test.js
+++ src/data/jobs/actions.test.js
@@ -6,13 +6,12 @@ import 'isomorphic-fetch'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import nock from 'nock'
+import urlJoin from 'url-join'
 import * as rootSelectors from '../../rootSelectors'
 import endpoints from '../../services/endpoints'
 import * as types from './actionTypes'
 import * as actions from './actions'
 import * as selectors from './reducer'
-
-const { origin, pathname } = new URL(endpoints.jobs)
 
 /**
  * Mocks
@@ -26,7 +25,7 @@ const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
 /**
- * Tests
+ * Fetch jobs
  */
 
 describe('fetchJobsSuccess', () => {
@@ -54,6 +53,8 @@ describe('fetchJobsFail', () => {
 })
 
 describe('fetchJobs', () => {
+    const { origin, pathname } = new URL(endpoints.jobs)
+
     beforeEach(() => {
         nock.disableNetConnect()
     })
@@ -113,6 +114,8 @@ describe('fetchJobs', () => {
 })
 
 describe('fetchJobsIfNeeded', () => {
+    const { origin, pathname } = new URL(endpoints.jobs)
+
     beforeEach(() => {
         nock.disableNetConnect()
     })
@@ -157,5 +160,165 @@ describe('fetchJobsIfNeeded', () => {
         return store
             .dispatch(actions.fetchJobsIfNeeded())
             .then(() => expect(store.getActions()).toEqual([]))
+    })
+})
+
+/**
+ * Enable job
+ */
+
+describe('enableJobSuccess', () => {
+    it('should create a ENABLE_JOB_SUCCESS action', () => {
+        const actual = actions.enableJobSuccess()
+        const expected = {
+            type: types.ENABLE_JOB_SUCCESS,
+        }
+
+        expect(actual).toEqual(expected)
+    })
+})
+
+describe('enableJobFail', () => {
+    it('should create a ENABLE_JOB_FAIL action', () => {
+        const error = new Error()
+        const actual = actions.enableJobFail(error)
+        const expected = {
+            type: types.ENABLE_JOB_FAIL,
+            error,
+        }
+
+        expect(actual).toEqual(expected)
+    })
+})
+
+describe('enableJob', () => {
+    const id = 'id'
+    const { origin, pathname } = new URL(urlJoin(endpoints.jobs, `/${id}`))
+
+    beforeEach(() => {
+        nock.disableNetConnect()
+    })
+
+    afterEach(() => {
+        nock.cleanAll()
+        nock.enableNetConnect()
+    })
+
+    it('should handle successful fetches', () => {
+        nock(origin)
+            .patch(pathname)
+            .reply(204)
+
+        const store = mockStore({})
+        const expectedActions = [
+            { type: types.ENABLE_JOB },
+            {
+                type: types.ENABLE_JOB_SUCCESS,
+            },
+        ]
+
+        return store
+            .dispatch(actions.enableJob(id))
+            .then(() => expect(store.getActions()).toEqual(expectedActions))
+    })
+
+    it('should handle unsuccessful fetches', () => {
+        const error = new Error('Internal Server Error')
+        nock(origin)
+            .patch(pathname)
+            .reply(500)
+
+        const store = mockStore({})
+        const expectedActions = [
+            { type: types.ENABLE_JOB },
+            {
+                type: types.ENABLE_JOB_FAIL,
+                error,
+            },
+        ]
+
+        return store
+            .dispatch(actions.enableJob(id))
+            .then(() => expect(store.getActions()).toEqual(expectedActions))
+    })
+})
+
+/**
+ * Disable job
+ */
+
+describe('disableJobSuccess', () => {
+    it('should create a DISABLE_JOB_SUCCESS action', () => {
+        const actual = actions.disableJobSuccess()
+        const expected = {
+            type: types.DISABLE_JOB_SUCCESS,
+        }
+
+        expect(actual).toEqual(expected)
+    })
+})
+
+describe('disableJobFail', () => {
+    it('should create a DISABLE_JOB_FAIL action', () => {
+        const error = new Error()
+        const actual = actions.disableJobFail(error)
+        const expected = {
+            type: types.DISABLE_JOB_FAIL,
+            error,
+        }
+
+        expect(actual).toEqual(expected)
+    })
+})
+
+describe('disableJob', () => {
+    const id = 'id'
+    const { origin, pathname } = new URL(urlJoin(endpoints.jobs, `/${id}`))
+
+    beforeEach(() => {
+        nock.disableNetConnect()
+    })
+
+    afterEach(() => {
+        nock.cleanAll()
+        nock.enableNetConnect()
+    })
+
+    it('should handle successful fetches', () => {
+        nock(origin)
+            .patch(pathname)
+            .reply(204)
+
+        const store = mockStore({})
+        const expectedActions = [
+            { type: types.DISABLE_JOB },
+            {
+                type: types.DISABLE_JOB_SUCCESS,
+            },
+        ]
+
+        return store
+            .dispatch(actions.disableJob(id))
+            .then(() => expect(store.getActions()).toEqual(expectedActions))
+    })
+
+    it('should handle unsuccessful fetches', () => {
+        const error = new Error('Internal Server Error')
+        nock(origin)
+            .patch(pathname)
+            .reply(500)
+
+        const store = mockStore({})
+        const expectedActions = [
+            { type: types.DISABLE_JOB },
+            {
+                type: types.DISABLE_JOB_FAIL,
+                error,
+            },
+        ]
+
+        return store
+            .dispatch(actions.disableJob(id))
+            .then(() => expect(store.getActions()).toEqual(expectedActions))
     })
 })
