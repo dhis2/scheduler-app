86dfc129e7b1cf8bc53e8e5403578ad66e3284a9
ismay <ismay@ismaywolff.nl>
2019-07-16 11:51:32 +0200

    chore: add cron validation

---
+++ src/services/validate-cron/validate-cron.test.js
@@ -0,0 +1,30 @@
+import validateCron from './validate-cron'
+
+describe('validateCron', () => {
+    it('should not allow an empty string', () => {
+        expect(validateCron('')).toEqual(false)
+    })
+
+    it('should allow a question mark for day-of-month', () => {
+        expect(validateCron('* * * ? * *')).toEqual(true)
+    })
+
+    it('should allow question mark for the day-of-week', () => {
+        expect(validateCron('* * * * * ?')).toEqual(true)
+    })
+
+    it('should disallow question mark in invalid locations', () => {
+        expect(validateCron('? * * * * *')).toEqual(false)
+        expect(validateCron('* ? * * * *')).toEqual(false)
+        expect(validateCron('* * ? * * *')).toEqual(false)
+        expect(validateCron('* * * * ? *')).toEqual(false)
+    })
+
+    it('should allow strings for day-of-week', () => {
+        expect(validateCron('* * * * * MON')).toEqual(true)
+    })
+
+    it('should allow string ranges for day-of-week', () => {
+        expect(validateCron('* * * * * MON-FRI')).toEqual(true)
+    })
+})
