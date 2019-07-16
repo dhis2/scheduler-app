fa9a5d54d2ea5d8debd603ec96c21a413aaf20d3
ismay <ismay@ismaywolff.nl>
2019-07-16 14:51:23 +0200

    chore: add rudimentary add job functionality

---
+++ src/pages/JobAdd/JobAddContainer.js
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react'
+import { func, string, bool } from 'prop-types'
+import { connect } from 'react-redux'
+import { CircularLoader } from '@dhis2/ui-core'
+import { actions, selectors } from '../../data/job-types'
+import { actions as jobActions } from '../../data/jobs'
+import * as rootSelectors from '../../rootSelectors'
+import { AbsoluteCenter } from '../../components/AbsoluteCenter'
+import JobAdd from './JobAdd'
+
+export const UnconnectedJobAddContainer = ({
+    didFetch,
+    errorMessage,
+    createJob,
+    fetchJobTypesIfNeeded,
+}) => {
+    useEffect(() => {
+        fetchJobTypesIfNeeded()
+    }, [fetchJobTypesIfNeeded])
+
+    // Show spinner when there are no jobs to display yet
+    if (!didFetch) {
+        return (
+            <AbsoluteCenter vertical>
+                <CircularLoader />
+                Loading form options
+            </AbsoluteCenter>
+        )
+    }
+
+    if (errorMessage) {
+        return <span>{errorMessage}</span>
+    }
+
+    return <JobAdd createJob={createJob} />
+}
+
+UnconnectedJobAddContainer.propTypes = {
+    didFetch: bool.isRequired,
+    isFetching: bool.isRequired,
+    errorMessage: string.isRequired,
+    createJob: func.isRequired,
+    fetchJobTypesIfNeeded: func.isRequired,
+}
+
+const mapStateToProps = state => {
+    const jobTypes = rootSelectors.getJobTypes(state)
+
+    return {
+        didFetch: selectors.getDidFetch(jobTypes),
+        isFetching: selectors.getIsFetching(jobTypes),
+        errorMessage: selectors.getErrorMessage(jobTypes),
+    }
+}
+
+const mapDispatchToProps = {
+    fetchJobTypesIfNeeded: actions.fetchJobTypesIfNeeded,
+    createJob: jobActions.createJob,
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(UnconnectedJobAddContainer)
