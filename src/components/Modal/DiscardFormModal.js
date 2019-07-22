b5e993d97b9ba2e37b3fba6a682d14e9dfba1640
ismay <ismay@ismaywolff.nl>
2019-07-22 15:02:36 +0200

    chore: add back to list jobs button

---
+++ src/components/Modal/DiscardFormModal.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { func } from 'prop-types'
+import { Button, Modal, ButtonStrip } from '@dhis2/ui-core'
+import { connect } from 'react-redux'
+import { actions as modalActions } from '../../data/modal'
+import history from '../../services/history'
+
+export const UnconnectedDiscardFormModal = ({ hideModal }) => (
+    <Modal open small onClose={hideModal}>
+        <Modal.Content>
+            Are you sure you want to discard this form?
+        </Modal.Content>
+        <Modal.Actions>
+            <ButtonStrip end>
+                <Button
+                    secondary
+                    name="cancel-discard-form"
+                    onClick={hideModal}
+                >
+                    Cancel
+                </Button>
+                <Button
+                    destructive
+                    name="discard-form"
+                    onClick={() => {
+                        history.push('/')
+                        hideModal()
+                    }}
+                >
+                    Discard
+                </Button>
+            </ButtonStrip>
+        </Modal.Actions>
+    </Modal>
+)
+
+UnconnectedDiscardFormModal.propTypes = {
+    hideModal: func.isRequired,
+}
+
+const mapDispatchToProps = {
+    hideModal: modalActions.hideModal,
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(UnconnectedDiscardFormModal)
