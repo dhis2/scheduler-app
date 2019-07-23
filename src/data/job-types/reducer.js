81b8a235548e7be492f07f036906e2c26e0b776c
ismay <ismay@ismaywolff.nl>
2019-07-23 14:34:59 +0200

    chore: add parameter selectors

--- src/data/job-types/reducer.js
+++ src/data/job-types/reducer.js
@@ -43,6 +43,41 @@ export const getJobTypes = state => {
     return Object.keys(state.data)
 }
 
+/**
+ * Returns the name, type and label of a parameter of a job. Useful to map it to an input field.
+ */
+
+export const getJobTypeParameter = (state, jobType, parameterName) => {
+    const data = state.data
+    const job = data[jobType]
+    const parameter = job[parameterName]
+
+    return {
+        name: parameter.name,
+        type: parameter.klass,
+        label: parameter.fieldName,
+    }
+}
+
+/**
+ * Returns an object with all parameters for a certain jobType
+ */
+
+export const getJobTypeParameters = (state, jobType) => {
+    const data = state.data
+    const parameterNames = Object.keys(data[jobType])
+
+    return parameterNames.reduce((parameters, parameterName) => {
+        parameters[parameterName] = getJobTypeParameter(
+            state,
+            jobType,
+            parameterName
+        )
+
+        return parameters
+    }, {})
+}
+
 export const getShouldFetch = state => {
     const { isFetching, errorMessage, lastUpdated } = state
     const hasError = !!errorMessage
