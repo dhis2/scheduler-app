06c1fbd4d014d50438140d39e39bb288a88cbd09
ismay <ismay@ismaywolff.nl>
2019-07-24 14:41:02 +0200

    chore: remove normalisation [ci skip]

--- src/rootSelectors.js
+++ src/rootSelectors.js
@@ -1,5 +1,4 @@
 export const getMe = state => state.me
 export const getJobs = state => state.jobs
 export const getJobTypes = state => state.jobTypes
-export const getEntities = state => state.entities
 export const getModal = state => state.modal
