e77e8e064c817cb53597f32899b63c0803cc435e
ismay <ismay@ismaywolff.nl>
2019-07-25 17:44:34 +0200

    chore: fetch parameter options dynamically

--- src/components/Form/ParameterSetField.js
+++ src/components/Form/ParameterSetField.js
@@ -1,5 +1,99 @@
-const ParameterSetField = () => {
-    return null
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { arrayOf, func, bool, string } from 'prop-types'
+import { Field } from 'react-final-form'
+import { SelectField } from '@dhis2/ui-core'
+import * as rootSelectors from '../../rootSelectors'
+import { selectors as jobTypeSelectors } from '../../data/job-types'
+import { actions, selectors } from '../../data/parameter-options'
+import { InlineError } from '../Errors'
+
+export const UnconnectedParameterSetField = ({
+    didFetch,
+    endpoint,
+    errorMessage,
+    fetchParameterOptionsIfNeeded,
+    jobType,
+    label,
+    name,
+    options,
+    parameterName,
+}) => {
+    useEffect(() => {
+        fetchParameterOptionsIfNeeded(endpoint, jobType, parameterName)
+    }, [fetchParameterOptionsIfNeeded, endpoint, jobType, parameterName])
+
+    // Show loading state when options are loading
+    if (!didFetch) {
+        return <SelectField label={label} loading />
+    }
+
+    if (errorMessage) {
+        return <InlineError message={errorMessage} />
+    }
+
+    return (
+        <Field
+            name={name}
+            render={({ input }) => (
+                <SelectField {...input} label={label}>
+                    {options.map(option => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </SelectField>
+            )}
+        />
+    )
 }
 
-export default ParameterSetField
+UnconnectedParameterSetField.propTypes = {
+    didFetch: bool.isRequired,
+    endpoint: string.isRequired,
+    errorMessage: string.isRequired,
+    fetchParameterOptionsIfNeeded: func.isRequired,
+    jobType: string.isRequired,
+    label: string.isRequired,
+    name: string.isRequired,
+    options: arrayOf(string).isRequired,
+    parameterName: string.isRequired,
+}
+
+const mapStateToProps = (state, ownProps) => {
+    const { jobType, parameterName } = ownProps
+    const jobTypes = rootSelectors.getJobTypes(state)
+    const parameterOptions = rootSelectors.getParameterOptions(state)
+
+    return {
+        endpoint: jobTypeSelectors.getParameterOptionEndpoint(
+            jobTypes,
+            jobType,
+            parameterName
+        ),
+        didFetch: selectors.getDidFetch(
+            parameterOptions,
+            jobType,
+            parameterName
+        ),
+        errorMessage: selectors.getErrorMessage(
+            parameterOptions,
+            jobType,
+            parameterName
+        ),
+        options: selectors.getParameterOptions(
+            parameterOptions,
+            jobType,
+            parameterName
+        ),
+    }
+}
+
+const mapDispatchToProps = {
+    fetchParameterOptionsIfNeeded: actions.fetchParameterOptionsIfNeeded,
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(UnconnectedParameterSetField)
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import me from './data/me'
 import jobs from './data/jobs'
 import jobTypes from './data/job-types'
+import parameterOptions from './data/parameter-options'
 import modal from './data/modal'
 
 export default combineReducers({
     me,
     jobs,
     jobTypes,
+    parameterOptions,
     modal,
 })
