99b895fd398f5f0ae5e8e2c91936c1707c74925a
ismay <ismay@ismaywolff.nl>
2019-07-30 11:53:34 +0200

    refactor: rename to parameter-set

--- src/rootSelectors.js
+++ src/rootSelectors.js
@@ -1,5 +1,5 @@
 export const getMe = state => state.me
 export const getJobs = state => state.jobs
 export const getJobTypes = state => state.jobTypes
-export const getParameterOptions = state => state.parameterOptions
+export const getParameterSet = state => state.parameterSet
 export const getModal = state => state.modal
