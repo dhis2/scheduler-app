871d473c4fe3ac556aa4719b89c661d941a7b29c
ismay <ismay@ismaywolff.nl>
2019-08-08 13:51:04 +0200

    chore: add cron preset button

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,6 +1,13 @@
 import DeleteJobButton from './DeleteJobButton'
+import ShowCronPresetButton from './ShowCronPresetButton'
 import DiscardFormButton from './DiscardFormButton'
 import LinkButton from './LinkButton'
 import RunJobButton from './RunJobButton'
 
-export { DeleteJobButton, DiscardFormButton, RunJobButton, LinkButton }
+export {
+    DeleteJobButton,
+    DiscardFormButton,
+    RunJobButton,
+    LinkButton,
+    ShowCronPresetButton,
+}
