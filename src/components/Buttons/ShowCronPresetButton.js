0414a39072d283d6fa95178a0956c972e672529f
ismay <ismay@ismaywolff.nl>
2019-08-08 13:51:04 +0200

    chore: add cron preset button

---
+++ src/components/Buttons/ShowCronPresetButton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { func } from 'prop-types'
+import { connect } from 'react-redux'
+import { Button } from '@dhis2/ui-core'
+import { actions } from '../../data/modal'
+import { modalTypes } from '../Modal'
+
+export const DumbShowCronPresetButton = ({ showModal }) => {
+    return (
+        <Button
+            primary
+            name="show-cron-preset-modal"
+            onClick={() => showModal({ type: modalTypes.CRON_PRESET })}
+        >
+            Choose from preset times
+        </Button>
+    )
+}
+
+DumbShowCronPresetButton.propTypes = {
+    showModal: func.isRequired,
+}
+
+const mapDispatchToProps = {
+    showModal: actions.showModal,
+}
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(DumbShowCronPresetButton)
