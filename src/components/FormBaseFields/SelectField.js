c29c3a01723697ec6edb17e0a4fc634e6a887368
ismay <ismay@ismaywolff.nl>
2019-08-27 11:35:32 +0200

    chore: spread rest props on form base fields

--- src/components/FormBaseFields/SelectField.js
+++ src/components/FormBaseFields/SelectField.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import { node, bool, shape, string, object } from 'prop-types'
+import { node, bool, shape, object } from 'prop-types'
 import { SelectField as CoreSelect, Help } from '@dhis2/ui-core'
 
-const SelectField = ({ input, meta, label, required, children }) => {
+const SelectField = ({ input, meta, children, ...rest }) => {
     const { touched, error } = meta
     const hasError = touched && !!error
 
     return (
         <React.Fragment>
-            <CoreSelect
-                {...input}
-                label={label}
-                error={hasError}
-                required={required}
-            >
+            <CoreSelect {...input} {...rest} error={hasError}>
                 {children}
             </CoreSelect>
             {hasError && <Help error>{meta.error.message}</Help>}
@@ -26,14 +21,12 @@ SelectField.defaultProps = {
 }
 
 SelectField.propTypes = {
-    label: string.isRequired,
     meta: shape({
         error: object,
         touched: bool.isRequired,
     }).isRequired,
     input: object.isRequired,
     children: node.isRequired,
-    required: bool,
 }
 
 export default SelectField
