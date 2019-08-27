0fe1847f7f67d9e7d96dea7dc76a666805a9558f
ismay <ismay@ismaywolff.nl>
2019-08-27 11:35:32 +0200

    chore: spread rest props on form base fields

--- src/components/FormBaseFields/Switch.js
+++ src/components/FormBaseFields/Switch.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { string, object } from 'prop-types'
+import { object } from 'prop-types'
 import { Switch as CoreSwitch } from '@dhis2/ui-core'
 
-const Switch = ({ input, label }) => {
-    return <CoreSwitch {...input} label={label} />
+const Switch = ({ input, ...rest }) => {
+    return <CoreSwitch {...input} {...rest} />
 }
 
 Switch.propTypes = {
-    label: string.isRequired,
     input: object.isRequired,
 }
 
