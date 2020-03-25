e0275b71f67f996761353957474fe7078e974e69
ismay <ismay@ismaywolff.nl>
2020-03-25 16:49:04 +0100

    refactor: fix next run time display

--- src/pages/JobList/JobListTableItem.js
+++ src/pages/JobList/JobListTableItem.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import moment from 'moment'
 import { bool, shape, string } from 'prop-types'
 import cronstrue from 'cronstrue'
 import { TableRow, TableCell } from '@dhis2/ui-core'
 import { ToggleJobSwitch } from '../../components/Switches'
 import JobListActions from './JobListActions'
 import JobStatus from './JobStatus'
-
-/**
- * The use of moment below is to display in how much time the task
- * will run again. This should always be in the future (so, 'in xxx
- * hours', etc). At the moment though, we sometimes receive timestamps
- * that are in the past, which will cause the displayed relative time
- * to refer to the past as well ('xxx hours ago', etc.).
- *
- * Besides that, according to the moment docs you would expect toNow()
- * to be the correct method, as all those examples are describing future
- * events. But using fromNow() actually results in future relative time,
- * so that could be a mistake in the moment docs.
- */
+import JobNextRun from './JobNextRun'
 
 const JobListTableItem = ({
     job: {
@@ -35,7 +22,12 @@ const JobListTableItem = ({
         <TableCell>{displayName}</TableCell>
         <TableCell>{jobType}</TableCell>
         <TableCell>{cronstrue.toString(cronExpression)}</TableCell>
-        <TableCell>{moment(nextExecutionTime).fromNow()}</TableCell>
+        <TableCell>
+            <JobNextRun
+                nextExecutionTime={nextExecutionTime}
+                enabled={enabled}
+            />
+        </TableCell>
         <TableCell>
             <JobStatus status={jobStatus} />
         </TableCell>
