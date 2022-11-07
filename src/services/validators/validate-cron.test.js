016b6ba8950a5607354ff23ede4c157330b908f6
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2022-11-07 14:10:30 +0100

    chore(validate cron test): use `toBe` for boolean expectations

--- src/services/validators/validate-cron.test.js
+++ src/services/validators/validate-cron.test.js
@@ -2,38 +2,38 @@ import validateCron from './validate-cron'
 
 describe('validateCron', () => {
     it('should not allow an empty string', () => {
-        expect(validateCron('')).toEqual(false)
+        expect(validateCron('')).toBe(false)
     })
 
     it('should allow a question mark for day-of-month', () => {
-        expect(validateCron('* * * ? * *')).toEqual(true)
+        expect(validateCron('* * * ? * *')).toBe(true)
     })
 
     it('should allow question mark for the day-of-week', () => {
-        expect(validateCron('* * * * * ?')).toEqual(true)
+        expect(validateCron('* * * * * ?')).toBe(true)
     })
 
     it('should disallow question mark in invalid locations', () => {
-        expect(validateCron('? * * * * *')).toEqual(false)
-        expect(validateCron('* ? * * * *')).toEqual(false)
-        expect(validateCron('* * ? * * *')).toEqual(false)
-        expect(validateCron('* * * * ? *')).toEqual(false)
+        expect(validateCron('? * * * * *')).toBe(false)
+        expect(validateCron('* ? * * * *')).toBe(false)
+        expect(validateCron('* * ? * * *')).toBe(false)
+        expect(validateCron('* * * * ? *')).toBe(false)
     })
 
     it('should allow strings for day-of-week', () => {
-        expect(validateCron('* * * * * MON')).toEqual(true)
+        expect(validateCron('* * * * * MON')).toBe(true)
     })
 
     it('should allow string ranges for day-of-week', () => {
-        expect(validateCron('* * * * * MON-FRI')).toEqual(true)
+        expect(validateCron('* * * * * MON-FRI')).toBe(true)
     })
 
     it('should allow for valid fractions', () => {
-        expect(validateCron('0 0 */3 * * *')).toEqual(true)
+        expect(validateCron('0 0 */3 * * *')).toBe(true)
     })
 
     it('should allow for number ranges', () => {
-        expect(validateCron('0 0 1-5 * * *')).toEqual(true)
+        expect(validateCron('0 0 1-5 * * *')).toBe(true)
     })
 
     it('should allow ranges in fraction numerators', () => {
