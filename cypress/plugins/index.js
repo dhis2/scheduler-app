749c0c8405ac72e0e84474d328e7a05b49333c4c
ismay <git.o8qty@simplelogin.com>
2023-02-22 16:12:15 +0100

    test(cypress): use cypress session for authentication

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,7 @@
 const {
-    networkShim,
-    chromeAllowXSiteCookies,
     cucumberPreprocessor,
 } = require('@dhis2/cypress-plugins')
 
 module.exports = (on, config) => {
-    networkShim(on)
-    chromeAllowXSiteCookies(on, config)
     cucumberPreprocessor(on, config)
 }
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,5 +1,31 @@
 import '@testing-library/cypress/add-commands'
-import { enableAutoLogin, enableNetworkShim } from '@dhis2/cypress-commands'
 
-enableAutoLogin()
-enableNetworkShim()
+// Custom commands
+Cypress.Commands.add('login', (user) => {
+    cy.session(
+        user,
+        () => {
+            cy.visit('/')
+
+            // Ensure we're on the login page
+            cy.findByRole('heading', { name: 'Please sign in' }).should('exist')
+
+            // Enter credentials
+            cy.get('input#server').type(user.server)
+            cy.get('input#j_username').type(user.name)
+            cy.get('input#j_password').type(user.password)
+            cy.findByRole('button', { name: 'Sign in' }).click()
+
+            // Wait until main route has been loaded
+            cy.findByRole('heading', { name: 'Scheduled jobs' }).should('exist')
+        },
+        {
+            validate: () => {
+                cy.visit('/')
+                cy.findByRole('heading', { name: 'Scheduled jobs' }).should(
+                    'exist'
+                )
+            },
+        }
+    )
+})
