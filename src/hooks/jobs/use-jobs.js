838b32773bf9fdbfbdd87b07b6d0f79a6e86adf0
ismay <git.o8qty@simplelogin.com>
2023-03-27 17:06:59 +0200

    chore: add use job by id hook for edit route

---
+++ src/hooks/jobs/use-jobs.js
@@ -0,0 +1,32 @@
+import { useDataQuery } from '@dhis2/app-runtime'
+
+const key = 'jobs'
+const query = {
+    [key]: {
+        resource: 'jobConfigurations',
+        params: {
+            fields: [
+                'created',
+                'id',
+                'lastExecuted',
+                'lastExecutedStatus',
+                'name',
+            ],
+        },
+    },
+}
+
+const useJobs = () => {
+    const fetch = useDataQuery(query)
+
+    // Remove nesting from data
+    if (fetch.data) {
+        const data = fetch.data[key].jobConfigurations
+
+        return { ...fetch, data }
+    }
+
+    return fetch
+}
+
+export default useJobs
