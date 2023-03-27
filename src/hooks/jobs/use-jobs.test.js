aaa7750e5297079d938067e8c888ee4640231857
ismay <git.o8qty@simplelogin.com>
2023-03-27 17:28:39 +0200

    chore: add job hooks tests

---
+++ src/hooks/jobs/use-jobs.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { renderHook } from '@testing-library/react-hooks'
+import { CustomDataProvider } from '@dhis2/app-runtime'
+import useJobs from './use-jobs'
+
+describe('useJobs', () => {
+    it('should return the expected data', () => {
+        const match = [{ id: 'id' }]
+        const data = {
+            jobConfigurations: {
+                jobConfigurations: match,
+            },
+        }
+
+        const wrapper = ({ children }) => (
+            <CustomDataProvider data={data}>{children}</CustomDataProvider>
+        )
+
+        const { result, waitFor } = renderHook(() => useJobs(), {
+            wrapper,
+        })
+
+        waitFor(() => {
+            expect(result.current).toMatchObject({
+                loading: false,
+                error: undefined,
+                data: match,
+            })
+        })
+    })
+})
