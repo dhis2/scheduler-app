1803ad1bc28d4371d0ad129c2ab1d9d62761229e
ismay <git.o8qty@simplelogin.com>
2023-03-29 17:12:38 +0200

    refactor: remove containers

--- src/pages/JobAdd/JobAddContainer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import JobAddContainer from './JobAddContainer'
-
-describe('<JobAddContainer>', () => {
-    it('renders without errors', () => {
-        shallow(<JobAddContainer />)
-    })
-})
