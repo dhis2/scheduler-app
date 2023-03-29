8a4f48979444fc77e3cfea5e7037a4c4e42d5de3
ismay <git.o8qty@simplelogin.com>
2023-03-29 12:09:02 +0200

    test(jest): fix jest tests

--- src/components/JobTable/RunJobAction.test.js
+++ src/components/JobTable/RunJobAction.test.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import { StoreContext } from '../Store'
 import RunJobAction from './RunJobAction'
 
 describe('<RunJobAction>', () => {
     it('shows the modal when MenuItem is clicked and the job is enabled', () => {
-        const store = {
-            jobs: [{ id: 'id', enabled: true }],
-        }
-        const wrapper = mount(
-            <StoreContext.Provider value={store}>
-                <RunJobAction id="id" />
-            </StoreContext.Provider>
-        )
+        const wrapper = mount(<RunJobAction id="id" enabled />)
 
         expect(wrapper.find('RunJobModal')).toHaveLength(0)
         wrapper.find('a').simulate('click')
@@ -20,14 +12,7 @@ describe('<RunJobAction>', () => {
     })
 
     it('does not show the modal when MenuItem is clicked and the job is disabled', () => {
-        const store = {
-            jobs: [{ id: 'id', enabled: false }],
-        }
-        const wrapper = mount(
-            <StoreContext.Provider value={store}>
-                <RunJobAction id="id" />
-            </StoreContext.Provider>
-        )
+        const wrapper = mount(<RunJobAction id="id" />)
 
         expect(wrapper.find('RunJobModal')).toHaveLength(0)
         wrapper.find('a').simulate('click')
