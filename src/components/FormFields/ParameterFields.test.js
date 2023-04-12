2f6e0899ee271d57c629453f7fa2487c449710fe
ismay <git.o8qty@simplelogin.com>
2023-04-12 14:50:35 +0200

    test(jest): fix parameter fields test

--- src/components/FormFields/ParameterFields.test.js
+++ src/components/FormFields/ParameterFields.test.js
@@ -1,27 +1,39 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { ReactFinalForm } from '@dhis2/ui'
-import { StoreContext } from '../Store'
+import { useJobTypeParameters } from '../../hooks/job-types'
+import { useParameterOption } from '../../hooks/parameter-options'
 import ParameterFields from './ParameterFields'
 
 const { Form } = ReactFinalForm
 
+jest.mock('../../hooks/job-types', () => ({
+    useJobTypeParameters: jest.fn(),
+}))
+
+jest.mock('../../hooks/parameter-options', () => ({
+    useParameterOption: jest.fn(),
+}))
+
 describe('<ParameterFields>', () => {
     it('returns null if there are no parameters', () => {
-        const store = { jobTypes: [{ jobType: 'jobType' }] }
+        useJobTypeParameters.mockImplementation(() => ({
+            loading: false,
+            error: undefined,
+            data: [],
+        }))
         const props = {
             jobType: 'jobType',
         }
+
         const wrapper = mount(
-            <StoreContext.Provider value={store}>
-                <Form onSubmit={() => {}}>
-                    {() => (
-                        <form>
-                            <ParameterFields {...props} />
-                        </form>
-                    )}
-                </Form>
-            </StoreContext.Provider>
+            <Form onSubmit={() => {}}>
+                {() => (
+                    <form>
+                        <ParameterFields {...props} />
+                    </form>
+                )}
+            </Form>
         )
 
         const children = wrapper.find('form').children()
@@ -30,36 +42,33 @@ describe('<ParameterFields>', () => {
     })
 
     it('returns the expected component for skipTableTypes', () => {
-        const store = {
-            jobTypes: [
+        useParameterOption.mockImplementation(() => ({
+            loading: false,
+            error: undefined,
+            data: [],
+        }))
+        useJobTypeParameters.mockImplementation(() => ({
+            loading: false,
+            error: undefined,
+            data: [
                 {
-                    jobType: 'jobType',
-                    jobParameters: [
-                        {
-                            fieldName: 'fieldName',
-                            name: 'skipTableTypes',
-                            klass: 'klass',
-                        },
-                    ],
+                    fieldName: 'fieldName',
+                    name: 'skipTableTypes',
+                    klass: 'klass',
                 },
             ],
-            parameterOptions: {
-                skipTableTypes: [],
-            },
-        }
+        }))
         const props = {
-            jobType: 'jobType',
+            jobType: 'skipTableTypes',
         }
         const wrapper = mount(
-            <StoreContext.Provider value={store}>
-                <Form onSubmit={() => {}}>
-                    {() => (
-                        <form>
-                            <ParameterFields {...props} />
-                        </form>
-                    )}
-                </Form>
-            </StoreContext.Provider>
+            <Form onSubmit={() => {}}>
+                {() => (
+                    <form>
+                        <ParameterFields {...props} />
+                    </form>
+                )}
+            </Form>
         )
 
         const component = wrapper.find('SkipTableTypesField')
@@ -68,33 +77,28 @@ describe('<ParameterFields>', () => {
     })
 
     it('returns the expected component for java.lang.String', () => {
-        const store = {
-            jobTypes: [
+        useJobTypeParameters.mockImplementation(() => ({
+            loading: false,
+            error: undefined,
+            data: [
                 {
-                    jobType: 'jobType',
-                    jobParameters: [
-                        {
-                            fieldName: 'fieldName',
-                            name: 'name',
-                            klass: 'java.lang.String',
-                        },
-                    ],
+                    fieldName: 'fieldName',
+                    name: 'name',
+                    klass: 'java.lang.String',
                 },
             ],
-        }
+        }))
         const props = {
             jobType: 'jobType',
         }
         const wrapper = mount(
-            <StoreContext.Provider value={store}>
-                <Form onSubmit={() => {}}>
-                    {() => (
-                        <form>
-                            <ParameterFields {...props} />
-                        </form>
-                    )}
-                </Form>
-            </StoreContext.Provider>
+            <Form onSubmit={() => {}}>
+                {() => (
+                    <form>
+                        <ParameterFields {...props} />
+                    </form>
+                )}
+            </Form>
         )
 
         const component = wrapper.find('InputFieldFF')
@@ -103,33 +107,28 @@ describe('<ParameterFields>', () => {
     })
 
     it('returns the expected component for java.lang.Boolean', () => {
-        const store = {
-            jobTypes: [
+        useJobTypeParameters.mockImplementation(() => ({
+            loading: false,
+            error: undefined,
+            data: [
                 {
-                    jobType: 'jobType',
-                    jobParameters: [
-                        {
-                            fieldName: 'fieldName',
-                            name: 'name',
-                            klass: 'java.lang.Boolean',
-                        },
-                    ],
+                    fieldName: 'fieldName',
+                    name: 'name',
+                    klass: 'java.lang.Boolean',
                 },
             ],
-        }
+        }))
         const props = {
             jobType: 'jobType',
         }
         const wrapper = mount(
-            <StoreContext.Provider value={store}>
-                <Form onSubmit={() => {}}>
-                    {() => (
-                        <form>
-                            <ParameterFields {...props} />
-                        </form>
-                    )}
-                </Form>
-            </StoreContext.Provider>
+            <Form onSubmit={() => {}}>
+                {() => (
+                    <form>
+                        <ParameterFields {...props} />
+                    </form>
+                )}
+            </Form>
         )
 
         const component = wrapper.find('SwitchFieldFF')
@@ -138,33 +137,28 @@ describe('<ParameterFields>', () => {
     })
 
     it('returns the expected component for java.lang.Integer', () => {
-        const store = {
-            jobTypes: [
+        useJobTypeParameters.mockImplementation(() => ({
+            loading: false,
+            error: undefined,
+            data: [
                 {
-                    jobType: 'jobType',
-                    jobParameters: [
-                        {
-                            fieldName: 'fieldName',
-                            name: 'name',
-                            klass: 'java.lang.Integer',
-                        },
-                    ],
+                    fieldName: 'fieldName',
+                    name: 'name',
+                    klass: 'java.lang.Integer',
                 },
             ],
-        }
+        }))
         const props = {
             jobType: 'jobType',
         }
         const wrapper = mount(
-            <StoreContext.Provider value={store}>
-                <Form onSubmit={() => {}}>
-                    {() => (
-                        <form>
-                            <ParameterFields {...props} />
-                        </form>
-                    )}
-                </Form>
-            </StoreContext.Provider>
+            <Form onSubmit={() => {}}>
+                {() => (
+                    <form>
+                        <ParameterFields {...props} />
+                    </form>
+                )}
+            </Form>
         )
 
         const component = wrapper.find('InputFieldFF')
@@ -173,36 +167,28 @@ describe('<ParameterFields>', () => {
     })
 
     it('returns the expected component for java.util.List', () => {
-        const store = {
-            jobTypes: [
+        useJobTypeParameters.mockImplementation(() => ({
+            loading: false,
+            error: undefined,
+            data: [
                 {
-                    jobType: 'jobType',
-                    jobParameters: [
-                        {
-                            fieldName: 'fieldName',
-                            name: 'parameterName',
-                            klass: 'java.util.List',
-                        },
-                    ],
+                    fieldName: 'fieldName',
+                    name: 'parameterName',
+                    klass: 'java.util.List',
                 },
             ],
-            parameterOptions: {
-                parameterName: [{ id: 'id', displayName: 'displayName' }],
-            },
-        }
+        }))
         const props = {
             jobType: 'jobType',
         }
         const wrapper = mount(
-            <StoreContext.Provider value={store}>
-                <Form onSubmit={() => {}}>
-                    {() => (
-                        <form>
-                            <ParameterFields {...props} />
-                        </form>
-                    )}
-                </Form>
-            </StoreContext.Provider>
+            <Form onSubmit={() => {}}>
+                {() => (
+                    <form>
+                        <ParameterFields {...props} />
+                    </form>
+                )}
+            </Form>
         )
 
         const component = wrapper.find('LabeledOptionsField')
