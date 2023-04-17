f76dd98e2498dba28af2866dd0389f2b9e87251e
ismay <git.o8qty@simplelogin.com>
2023-04-17 16:28:14 +0200

    style(eslint): remove unused store context export

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,5 +1,4 @@
 import Store from './Store'
-import StoreContext from './StoreContext'
 import { useJobFilter, useShowSystemJobs } from './hooks'
 
-export { Store, StoreContext, useJobFilter, useShowSystemJobs }
+export { Store, useJobFilter, useShowSystemJobs }
