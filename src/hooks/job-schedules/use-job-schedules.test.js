6002daa3bb7d24316565e33e2f2c2131b39e71ff
ismay <git.o8qty@simplelogin.com>
2023-04-24 17:42:16 +0200

    refactor: handle malformed data for job schedules

--- src/hooks/job-schedules/use-job-schedules.test.js
+++ src/hooks/job-schedules/use-job-schedules.test.js
@@ -4,6 +4,25 @@ import { CustomDataProvider } from '@dhis2/app-runtime'
 import useJobSchedules from './use-job-schedules'
 
 describe('useJobSchedules', () => {
+    it('should return the expected loading state', () => {
+        const data = { scheduler: [] }
+        const wrapper = ({ children }) => (
+            <CustomDataProvider data={data}>{children}</CustomDataProvider>
+        )
+
+        const { result, waitFor } = renderHook(() => useJobSchedules(), {
+            wrapper,
+        })
+
+        waitFor(() => {
+            expect(result.current).toMatchObject({
+                loading: true,
+                error: undefined,
+                data: [],
+            })
+        })
+    })
+
     it('should return the expected data', () => {
         const job = { sequence: [{ id: 'id' }] }
         const data = { scheduler: [job] }
@@ -20,6 +39,66 @@ describe('useJobSchedules', () => {
                 loading: false,
                 error: undefined,
                 data: [{ id: 'id', sequence: [{ id: 'id' }] }],
+            })
+        })
+    })
+
+    it('should not fail if sequence is missing', () => {
+        const job = {}
+        const data = { scheduler: [job] }
+        const wrapper = ({ children }) => (
+            <CustomDataProvider data={data}>{children}</CustomDataProvider>
+        )
+
+        const { result, waitFor } = renderHook(() => useJobSchedules(), {
+            wrapper,
+        })
+
+        waitFor(() => {
+            expect(result.current).toMatchObject({
+                loading: false,
+                error: undefined,
+                data: [job],
+            })
+        })
+    })
+
+    it('should not fail if sequence is empty', () => {
+        const job = { sequence: [] }
+        const data = { scheduler: [job] }
+        const wrapper = ({ children }) => (
+            <CustomDataProvider data={data}>{children}</CustomDataProvider>
+        )
+
+        const { result, waitFor } = renderHook(() => useJobSchedules(), {
+            wrapper,
+        })
+
+        waitFor(() => {
+            expect(result.current).toMatchObject({
+                loading: false,
+                error: undefined,
+                data: [job],
+            })
+        })
+    })
+
+    it('should not fail if the first sequence has no id', () => {
+        const job = { sequence: [{}] }
+        const data = { scheduler: [job] }
+        const wrapper = ({ children }) => (
+            <CustomDataProvider data={data}>{children}</CustomDataProvider>
+        )
+
+        const { result, waitFor } = renderHook(() => useJobSchedules(), {
+            wrapper,
+        })
+
+        waitFor(() => {
+            expect(result.current).toMatchObject({
+                loading: false,
+                error: undefined,
+                data: [job],
             })
         })
     })
