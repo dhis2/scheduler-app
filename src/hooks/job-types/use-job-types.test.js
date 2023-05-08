d94d2abcb1bfcc56a2853f79b07db11687bebe31
ismay <git.o8qty@simplelogin.com>
2023-05-08 17:58:07 +0200

    test(hooks): improve hooks test coverage

--- src/hooks/job-types/use-job-types.test.js
+++ src/hooks/job-types/use-job-types.test.js
@@ -23,4 +23,29 @@ describe('useJobTypes', () => {
             })
         })
     })
+
+    it('should return an error if job types are in an unexpected format', async () => {
+        const data = {
+            'jobConfigurations/jobTypes': {
+                jobTypes: false,
+            },
+        }
+        const wrapper = ({ children }) => (
+            <CustomDataProvider data={data}>{children}</CustomDataProvider>
+        )
+
+        const { result, waitFor } = renderHook(() => useJobTypes(), {
+            wrapper,
+        })
+
+        await waitFor(() => {
+            expect(result.current).toMatchObject({
+                loading: false,
+                data: undefined,
+            })
+            expect(result.current.error.message).toBe(
+                'Did not receive the expected job types'
+            )
+        })
+    })
 })
--- src/hooks/parameter-options/use-parameter-options.test.js
+++ src/hooks/parameter-options/use-parameter-options.test.js
@@ -38,4 +38,32 @@ describe('useParameterOptions', () => {
             })
         })
     })
+
+    it('should return an error if the parameter options are in an unexpected format', async () => {
+        const data = {
+            'analytics/tableTypes': 'skipTableTypes',
+            validationRuleGroups: {},
+            pushAnalysis: { pushAnalysis: 'pushAnalysis' },
+            predictors: { predictors: 'predictors' },
+            predictorGroups: { predictorGroups: 'predictorGroups' },
+            dataIntegrity: 'dataIntegrityChecks',
+        }
+        const wrapper = ({ children }) => (
+            <CustomDataProvider data={data}>{children}</CustomDataProvider>
+        )
+
+        const { result, waitFor } = renderHook(() => useParameterOptions(), {
+            wrapper,
+        })
+
+        await waitFor(() => {
+            expect(result.current).toMatchObject({
+                loading: false,
+                data: undefined,
+            })
+            expect(result.current.error.message).toBe(
+                'Did not receive the expected parameter options'
+            )
+        })
+    })
 })
