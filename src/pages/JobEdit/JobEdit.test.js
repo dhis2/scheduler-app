5297eb91cc22f836c8b1b2d3cbc0c1997fe1aa51
ismay <git.o8qty@simplelogin.com>
2023-05-08 17:04:46 +0200

    test(pages): improve page test coverage

--- src/pages/JobEdit/JobEdit.test.js
+++ src/pages/JobEdit/JobEdit.test.js
@@ -1,37 +1,63 @@
 import React from 'react'
-import { mount } from 'enzyme'
-import { CustomDataProvider } from '@dhis2/app-runtime'
+import { shallow } from 'enzyme'
 import { useParams } from 'react-router-dom'
+import { useJobById } from '../../hooks/jobs'
 import JobEdit from './JobEdit'
 
 jest.mock('react-router-dom', () => ({
     useParams: jest.fn(),
 }))
 
+jest.mock('../../hooks/jobs', () => ({
+    useJobById: jest.fn(),
+}))
+
 describe('<JobEdit>', () => {
-    it('renders without errors', () => {
+    it('renders a spinner when loading the requested job', () => {
         const id = 'id'
-        const data = {
-            jobConfigurations: {
-                jobConfigurations: [
-                    {
-                        id,
-                        name: 'name',
-                        created: 'created',
-                        lastExecutedStatus: 'lastExecutedStatus',
-                        lastExecuted: 'lastExecuted',
-                    },
-                ],
-            },
-            'jobConfigurations/jobTypes': { jobTypes: [] },
-        }
 
         useParams.mockImplementation(() => id)
+        useJobById.mockImplementation(() => ({ fetching: true }))
 
-        mount(
-            <CustomDataProvider data={data}>
-                <JobEdit />
-            </CustomDataProvider>
-        )
+        const wrapper = shallow(<JobEdit />)
+        const spinner = wrapper.find('Spinner')
+
+        expect(spinner).toHaveLength(1)
+    })
+
+    it('renders errors encountered during fetching', () => {
+        const id = 'id'
+
+        useParams.mockImplementation(() => id)
+        useJobById.mockImplementation(() => ({
+            fetching: false,
+            error: new Error('Something went wrong'),
+        }))
+
+        const wrapper = shallow(<JobEdit />)
+        const noticebox = wrapper.find('NoticeBox')
+
+        expect(noticebox).toHaveLength(1)
+    })
+
+    it('renders without errors when loading has completed', () => {
+        const id = 'id'
+
+        useParams.mockImplementation(() => id)
+        useJobById.mockImplementation(() => ({
+            fetching: false,
+            error: undefined,
+            data: {
+                name: '',
+                created: '',
+                lastExecutedStatus: '',
+                lastExecuted: '',
+            },
+        }))
+
+        const wrapper = shallow(<JobEdit />)
+        const jobform = wrapper.find('JobEditFormContainer')
+
+        expect(jobform).toHaveLength(1)
     })
 })
--- src/pages/JobView/JobView.test.js
+++ src/pages/JobView/JobView.test.js
@@ -1,39 +1,65 @@
 import React from 'react'
-import { mount } from 'enzyme'
-import { CustomDataProvider } from '@dhis2/app-runtime'
+import { shallow } from 'enzyme'
 import { useParams } from 'react-router-dom'
+import { useJobById } from '../../hooks/jobs'
 import JobView from './JobView'
 
 jest.mock('react-router-dom', () => ({
     useParams: jest.fn(),
 }))
 
+jest.mock('../../hooks/jobs', () => ({
+    useJobById: jest.fn(),
+}))
+
 describe('<JobView>', () => {
-    it('renders without errors', () => {
+    it('renders a spinner when loading the requested job', () => {
         const id = 'id'
-        const data = {
-            jobConfigurations: {
-                jobConfigurations: [
-                    {
-                        id,
-                        name: 'name',
-                        created: 'created',
-                        lastExecutedStatus: 'lastExecutedStatus',
-                        lastExecuted: 'lastExecuted',
-                        jobType: 'jobType',
-                        cronExpression: 'cronExpression',
-                    },
-                ],
-            },
-            'jobConfigurations/jobTypes': { jobTypes: [] },
-        }
 
         useParams.mockImplementation(() => id)
+        useJobById.mockImplementation(() => ({ fetching: true }))
 
-        mount(
-            <CustomDataProvider data={data}>
-                <JobView />
-            </CustomDataProvider>
-        )
+        const wrapper = shallow(<JobView />)
+        const spinner = wrapper.find('Spinner')
+
+        expect(spinner).toHaveLength(1)
+    })
+
+    it('renders errors encountered during fetching', () => {
+        const id = 'id'
+
+        useParams.mockImplementation(() => id)
+        useJobById.mockImplementation(() => ({
+            fetching: false,
+            error: new Error('Something went wrong'),
+        }))
+
+        const wrapper = shallow(<JobView />)
+        const noticebox = wrapper.find('NoticeBox')
+
+        expect(noticebox).toHaveLength(1)
+    })
+
+    it('renders without errors when loading has completed', () => {
+        const id = 'id'
+
+        useParams.mockImplementation(() => id)
+        useJobById.mockImplementation(() => ({
+            fetching: false,
+            error: undefined,
+            data: {
+                name: '',
+                created: '',
+                lastExecutedStatus: '',
+                lastExecuted: '',
+                jobType: 'DATA_INTEGRITY',
+                cronExpression: '',
+            },
+        }))
+
+        const wrapper = shallow(<JobView />)
+        const jobform = wrapper.find('JobDetails')
+
+        expect(jobform).toHaveLength(1)
     })
 })
