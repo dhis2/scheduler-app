5ec3bcc4fe30773b64f1e7aca0cdff702e8fcc1e
ismay <git.o8qty@simplelogin.com>
2023-06-12 15:06:04 +0200

    feat: handle order field selections

--- src/components/FormFields/SequenceOrderField.js
+++ src/components/FormFields/SequenceOrderField.js
@@ -10,21 +10,24 @@ import i18n from '@dhis2/d2-i18n'
 import { useQueueables } from '../../hooks/queueables'
 
 const { Field } = ReactFinalForm
-const { arrayOf, object } = PropTypes
+const { arrayOf, object, shape, func, array } = PropTypes
 
 // Customized transfer component used in the form field below
-const SequenceTransfer = ({ options }) => {
+const SequenceTransfer = ({ options, input }) => {
     const headerStyle = {
         margin: '0.5em 0',
         fontWeight: 500,
     }
 
+    const { onChange, value } = input
+
     return (
         <Transfer
             options={options}
+            selected={value}
             filterable
             enableOrderChange
-            onChange={() => {}}
+            onChange={({ selected }) => onChange(selected)}
             leftHeader={<h4 style={headerStyle}>Available jobs</h4>}
             rightHeader={<h4 style={headerStyle}>Jobs in this sequence</h4>}
         />
@@ -32,12 +35,17 @@ const SequenceTransfer = ({ options }) => {
 }
 
 SequenceTransfer.propTypes = {
+    input: shape({
+        onChange: func.isRequired,
+        value: array.isRequired,
+    }).isRequired,
     options: arrayOf(object).isRequired,
 }
 
 // The key under which this field will be sent to the backend
 const FIELD_NAME = 'sequence'
 const VALIDATOR = composeValidators(hasValue)
+const initialValue = []
 
 // Form field for ordering jobs in a queue
 const SequenceOrderField = () => {
@@ -54,6 +62,7 @@ const SequenceOrderField = () => {
             name={FIELD_NAME}
             component={SequenceTransfer}
             options={options}
+            initialValue={initialValue}
             label={i18n.t('Job sequence')}
             validate={VALIDATOR}
             required
