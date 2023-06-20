d51b394aba849edd8d5ae2428bace3cc1881eb36
ismay <git.o8qty@simplelogin.com>
2023-06-20 12:21:00 +0200

    test(submit-job-queue): add tests

---
+++ src/hooks/job-queues/use-submit-job-queue.test.js
@@ -0,0 +1,63 @@
+import { useDataEngine } from '@dhis2/app-runtime'
+import history from '../../services/history'
+import formatError from '../../services/format-error'
+import useSubmitJobQueue from './use-submit-job-queue'
+
+jest.mock('@dhis2/app-runtime', () => ({
+    useDataEngine: jest.fn(),
+}))
+
+jest.mock('../../services/history', () => ({
+    push: jest.fn(),
+}))
+
+jest.mock('../../services/format-error', () => jest.fn())
+
+describe('useSubmitJobQueue', () => {
+    it('should redirect to root', () => {
+        const engine = {
+            mutate: () => Promise.resolve(),
+        }
+        useDataEngine.mockImplementation(() => engine)
+        const [submitJobQueue] = useSubmitJobQueue()
+
+        expect.assertions(1)
+
+        submitJobQueue({ name: 'name' }).then(() => {
+            expect(history.push).toHaveBeenCalledWith('/')
+        })
+    })
+
+    it('should resolve with errors on validation errors', () => {
+        // Validation errors are detected by type
+        const error = new Error('Validation error')
+        error.type = 'access'
+
+        const engine = {
+            mutate: () => Promise.reject(error),
+        }
+        useDataEngine.mockImplementation(() => engine)
+        formatError.mockImplementation((error) => error)
+
+        const [submitJobQueue] = useSubmitJobQueue()
+
+        expect.assertions(1)
+
+        return expect(submitJobQueue({ name: 'name' })).resolves.toBe(error)
+    })
+
+    it('should reject with an error on any other errors', () => {
+        const error = new Error('Network error')
+
+        const engine = {
+            mutate: () => Promise.reject(error),
+        }
+        useDataEngine.mockImplementation(() => engine)
+
+        const [submitJobQueue] = useSubmitJobQueue()
+
+        expect.assertions(1)
+
+        return expect(submitJobQueue({ name: 'name' })).rejects.toBe(error)
+    })
+})
