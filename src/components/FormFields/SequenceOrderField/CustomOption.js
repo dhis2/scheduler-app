b587ef11589204f14b9550842efd7e8811d58c5d
ismay <git.o8qty@simplelogin.com>
2023-06-28 09:45:57 +0200

    feat: add double click handler for transfer custom option

--- src/components/FormFields/SequenceOrderField/CustomOption.js
+++ src/components/FormFields/SequenceOrderField/CustomOption.js
@@ -5,13 +5,21 @@ import styles from './CustomOption.module.css'
 
 const { bool, func, string } = PropTypes
 
-const CustomOption = ({ label, value, type, onClick, highlighted }) => {
+const CustomOption = ({
+    label,
+    value,
+    type,
+    onClick,
+    highlighted,
+    onDoubleClick,
+}) => {
     const className = cx(styles.wrapper, { [styles.highlighted]: highlighted })
 
     return (
         // eslint-disable-next-line jsx-a11y/no-static-element-interactions,jsx-a11y/click-events-have-key-events
         <div
             onClick={(event) => onClick({ label, value }, event)}
+            onDoubleClick={(event) => onDoubleClick({ label, value }, event)}
             className={className}
             data-value={value}
             data-test="dhis2-uicore-transferoption"
@@ -28,6 +36,7 @@ CustomOption.propTypes = {
     type: string.isRequired,
     value: string.isRequired,
     onClick: func.isRequired,
+    onDoubleClick: func.isRequired,
 }
 
 export default CustomOption
