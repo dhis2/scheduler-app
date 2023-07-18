492fe47785dc3a8f45f7278e78c7b3f8e15cb6c0
ismay <git.o8qty@simplelogin.com>
2023-07-18 17:20:22 +0200

    chore: update edit sequence form container

--- src/components/Forms/SequenceEditFormContainer.js
+++ src/components/Forms/SequenceEditFormContainer.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ReactFinalForm } from '@dhis2/ui'
+import { useParams } from 'react-router-dom'
 import history from '../../services/history'
 import { useSubmitJobQueue } from '../../hooks/job-queues'
 import SequenceEditForm from './SequenceEditForm'
 
 const { Form } = ReactFinalForm
 
-const SequenceEditFormContainer = ({ setIsPristine }) => {
+/**
+ * The fields we need for the initialValues for our form fields. Since we use
+ * these values to set the initial values in final-form, if we wouldn't filter
+ * them we'd end up submitting way more data than we intend to.
+ */
+
+const initialFields = [
+    'cronExpression',
+    'sequence',
+    'name',
+]
+
+const SequenceEditFormContainer = ({ sequence, setIsPristine }) => {
+    const { id } = useParams()
     const redirect = () => {
         history.push('/')
     }
     const [submitJobQueue] = useSubmitJobQueue({ onSuccess: redirect })
+
+    // Creating an object with just the values we want to use as initial values
+    const initialValues = initialFields.reduce((filtered, key) => {
+        filtered[key] = sequence[key]
+        return filtered
+    }, {})
 
     /**
      * destroyOnUnregister is enabled so that dynamic fields will be unregistered
@@ -19,10 +39,12 @@ const SequenceEditFormContainer = ({ setIsPristine }) => {
      */
     return (
         <Form
-            onSubmit={submitJobQueue}
             component={SequenceEditForm}
-            setIsPristine={setIsPristine}
             destroyOnUnregister
+            id={id}
+            initialValues={initialValues}
+            onSubmit={submitJobQueue}
+            setIsPristine={setIsPristine}
         />
     )
 }
