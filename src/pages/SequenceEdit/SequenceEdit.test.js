0855bb058aefa87c0965acc2f3f8cb44ff51335c
ismay <git.o8qty@simplelogin.com>
2023-07-20 16:03:18 +0200

    chore: remove placeholder tests

--- src/pages/SequenceEdit/SequenceEdit.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import SequenceEdit from './SequenceEdit'
-
-describe('<SequenceEdit>', () => {
-    it('renders without errors', () => {
-        shallow(<SequenceEdit />)
-    })
-})
