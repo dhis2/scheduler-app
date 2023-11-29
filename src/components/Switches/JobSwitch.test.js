07960948ac7a227d27445861f207d57e15118d78
ismay <git.login@ismay.email>
2023-11-29 11:51:49 +0100

    refactor: rely on cypress for jobswitch test

--- src/components/Switches/JobSwitch.test.js
+++ src/components/Switches/JobSwitch.test.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import waitForExpect from 'wait-for-expect'
-import { shallow, mount } from 'enzyme'
-import { CustomDataProvider } from '@dhis2/app-runtime'
+import { shallow } from 'enzyme'
 import JobSwitch from './JobSwitch'
 
 describe('<JobSwitch>', () => {
@@ -14,67 +12,5 @@ describe('<JobSwitch>', () => {
                 refetch={() => {}}
             />
         )
-    })
-
-    it('enables an inactive job and refetches when toggle is clicked', async () => {
-        const answerSpy = jest.fn(() => 'response')
-        const refetchSpy = jest.fn(() => Promise.resolve())
-
-        const props = {
-            id: 'id',
-            checked: false,
-            disabled: false,
-            refetch: refetchSpy,
-        }
-        const data = { 'jobConfigurations/id/enable': answerSpy }
-        const wrapper = mount(<JobSwitch {...props} />, {
-            wrappingComponent: CustomDataProvider,
-            wrappingComponentProps: { data },
-        })
-
-        wrapper
-            .find('input')
-            .find({ name: 'toggle-job-id' })
-            .simulate('change', { target: { checked: !props.checked } })
-
-        await waitForExpect(() => {
-            expect(answerSpy).toHaveBeenCalledWith(
-                'create',
-                expect.anything(),
-                expect.anything()
-            )
-            expect(refetchSpy).toHaveBeenCalled()
-        })
-    })
-
-    it('disables an active job and refetches when toggle is clicked', async () => {
-        const answerSpy = jest.fn(() => 'response')
-        const refetchSpy = jest.fn(() => Promise.resolve())
-
-        const props = {
-            id: 'id',
-            checked: true,
-            disabled: false,
-            refetch: refetchSpy,
-        }
-        const data = { 'jobConfigurations/id/disable': answerSpy }
-        const wrapper = mount(<JobSwitch {...props} />, {
-            wrappingComponent: CustomDataProvider,
-            wrappingComponentProps: { data },
-        })
-
-        wrapper
-            .find('input')
-            .find({ name: 'toggle-job-id' })
-            .simulate('change', { target: { checked: !props.checked } })
-
-        await waitForExpect(() => {
-            expect(answerSpy).toHaveBeenCalledWith(
-                'create',
-                expect.anything(),
-                expect.anything()
-            )
-            expect(refetchSpy).toHaveBeenCalled()
-        })
     })
 })
